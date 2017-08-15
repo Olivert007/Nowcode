6cd4b86c9aff374064854266786bfb3ab8e2db39
branch <522839943@qq.com>
2017-08-15 14:25:22 +0800

    Create 彩色的砖块.cpp

---
+++ 彩色的砖块.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	vector<int> NumOfEach(26);
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i]<'A' || s[i]>'Z') {
+			cout << 0;
+			return 0;
+		}
+		int index = s[i];
+		NumOfEach[index-65]++;
+	}
+	int count = 0;
+	for (int i = 0; i < 26; i++) {
+		if (NumOfEach[i] != 0)
+			count++;
+	}
+	
+	if (count == 0)
+		cout << 0;
+	if (count == 1)
+		cout << 1;
+	if (count == 2)
+		cout << 2;
+    if (count>2)
+        cout<<0;
+
+	system("pause");
+	return 0;
+}
